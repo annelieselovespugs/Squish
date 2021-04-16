b3b5a462424cefde70f8a29e2c92c4de41aaa925
“annelieselovespugs” <70218188+annelieselovespugs@users.noreply.github.com>
2021-04-16 12:06:53 -0700

    Added CloseToBoundaryA
    （＾・ﻌ・＾✿）

--- squishA.py
+++ squishA.py
@@ -1,8 +1,8 @@
 import numpy as np
-
 # Command + Shift + P   =>  type 'run'  and select 'Run script'
 # Command + I => runs script
 
+############################
 
 #Takes in a single number as an input between -100 and 100
 #Outputs a number between 0 and 1 relative to the input number size
@@ -19,5 +19,31 @@ def squishA (value):
     print output
 
 #test loop gives squishA every number from -100 to 100 (inclusive)
-for x in range (0,201):
-    squishA(x-100)
+#for x in range (0,201):
+    #squishA(x-100)
+
+
+
+############################
+
+
+#Takes in a single number as an input between 0 and 1
+#Outputs a number between 0.25 and 0 | 0 and 0.25
+#Outputs a number which is greatest when inputting 0.5 and smallest when inputting 0 or 1
+#Any inputs < 0.5 produce decreasing outputs as the inputs decrease from 0.5 to 0
+#Any inputs > 0.5 also produce decreasing outputs as the inputs increase from 0.5 to 1
+#A specific input should always calculate the same output
+#Every output has two unique inputs
+def CloseToBoundaryA (value):
+    if value > 0.5:
+        value = value - 0.5
+        output = 0.25 - (value / 2.)
+
+    else:
+        output = (value / 2.)
+
+    print output
+
+
+for x in range (0,101):
+    CloseToBoundaryA(x/100.)
